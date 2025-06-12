b917dabbfab55b90e9e1bee73ed0b0e30371bbc8
Widya Ardianto <nartos9090@gmail.com>
2025-06-12 16:56:02 +0700

    fix build error

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 
-import { cn } from "@/lib/utils"
+import { cn } from "../../libs/cn/utils"
 import { cva, type VariantProps } from "class-variance-authority"
 import { AnimatePresence, motion } from "motion/react"
 
